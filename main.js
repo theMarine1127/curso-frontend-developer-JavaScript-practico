207b7aabd05aef4f04a5072c77255ede9cc24b22
Jhon D. Marin <j.hondmarin@hotmail.com>
2024-02-12 22:04:01 -0500

    fusion de menu mobile, se agrego el menu mobile al html, los estiklos css y codigo jS

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 /**primero crea una variable seleccioando la clase */
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const menuHamIcon = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 /**luego usa el evento toggle para que aparesca y desaparesca el menu */
 menuEmail.addEventListener('click',toggleDesktopMenu);
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
 /**luego crea la funcion con el nombre del metodo toggleDesktopMenu */
 /**La funcion que quita o muestra el menu */
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive');
 }
