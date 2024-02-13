38609c30048b3765d4e2ca60cdf674d0b6f5efc3
Jhon D. Marin <j.hondmarin@hotmail.com>
2024-02-12 23:11:33 -0500

    se agrego el codigo html de carrito compras,styles css y codigo JS

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuHamIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCarritoIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 /**luego usa el evento toggle para que aparesca y desaparesca el menu */
 menuEmail.addEventListener('click',toggleDesktopMenu);
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+menuCarritoIcon.addEventListener('click',toggleCarritoAside);
 
 /**luego crea la funcion con el nombre del metodo toggleDesktopMenu */
 /**La funcion que quita o muestra el menu */
@@ -14,5 +17,19 @@ function toggleDesktopMenu() {
 }
 
 function toggleMobileMenu() {
+    const isAsideClosed = aside.classList.contains('inactive');
+    if(!isAsideClosed) {
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
+}
+
+function toggleCarritoAside() {
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    
+    if( !isMobileMenuClosed) {
+        mobileMenu.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
 }
